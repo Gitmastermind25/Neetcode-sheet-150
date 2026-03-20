9afa6bb5f2bb98c5b919997386a5a754adb35523
Yogita Pandit <yogitapandit2505@gmail.com>
2026-03-20 21:58:08 +0530

    Initial commit

---
+++ src/arraysandhashing/ValidAnagrams.java
@@ -0,0 +1,42 @@
+package arraysandhashing;
+
+import java.util.*;
+
+public class ValidAnagrams {
+
+    public static boolean isAnagram(String s, String t) {
+        int[] count = new int[26];
+
+        for (char ch : s.toCharArray()) {
+            count[ch - 'a']++;
+        }
+
+        for (char ch : t.toCharArray()) {
+            count[ch - 'a']--;
+        }
+
+        for (int num : count) {
+            if (num != 0) return false;
+        }
+
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Taking input
+        System.out.print("Enter first string: ");
+        String s = sc.nextLine();
+
+        System.out.print("Enter second string: ");
+        String t = sc.nextLine();
+
+        // Calling function
+        boolean result = isAnagram(s, t);
+
+        // Output
+        System.out.println("Is Anagram: " + result);
+
+        sc.close();
+    }
+}
